9e82aba47b0f12f000cddd481123154aafb664e3
sleklere <santiagoleklere@gmail.com>
2023-01-15 19:39:01 -0300

    header profile link

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -2,20 +2,28 @@ import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 const Header = (props) => {
+  // Use Redux for app-wide state 'userLoggedIn'
+
   return (
     <header className={styles.header}>
       <Link to={"/"} className={styles["nav-link"]}>
         App Name
       </Link>
       <nav>
-        {/* <Link to={"#"} className={styles["nav-link"]}>
-          About
-        </Link> */}
-        <Link className="btn">Register</Link>
+        {/* <Link className="btn">Register</Link>
         <Link to={"/login"} className="btn">
-          Login
+        Login
+      </Link> */}
+        <Link
+          to={"#"}
+          className={`${styles["profile-link"]} ${styles["nav-link"]}`}
+        >
+          <div className={styles["nav-profile-img"]}></div>
+          Profile
         </Link>
-        {/* <a href="#">Profile</a> */}
+        <Link to={"#"} className="btn">
+          Logout
+        </Link>
       </nav>
     </header>
   );
